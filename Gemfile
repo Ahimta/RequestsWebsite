79ddcba81ea686e2b0e69a80a8132d5b71609e4a
Abdullah Alansari <ahimta@gmail.com>
2013-05-13 18:10:14 +0300

    base

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'cucumber-rails', require: false
+  gem 'cucumber-rails-training-wheels'
+  gem 'database_cleaner'
+  gem 'capybara'
+  gem 'launchy'
+  gem 'rspec-rails'
+  gem 'pickle'
+end
+
+#group :production do
+#	gem 'pg'
+#end
+
+#gem 'jquery-rails'
+gem 'bcrypt-ruby' # To use ActiveModel has_secure_password
+gem 'debugger' # To use debugger
+gem 'haml'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RequestsWebsite::Application.config.secret_token = '92b8a6f8cf84077f96308ed2e1b85d04d6fa0611bc7a4bc065cfd4c5a9e9847cbfb01aa5c91f67227f65814c88be640b16522b60a43cf1099ee62b476199d788'

1111cb7d9866963faacff2532ea446300f5b69e6
Abdullah Alansari <ahimta@gmail.com>
2013-06-09 08:56:41 +0300

    nothing

--- db/migrate/20130513151028_create_users.rb
+++ db/migrate/20130513151028_create_users.rb
@@ -1,14 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :username
-      t.string :password_digest
-      t.boolean :admin
-      t.belongs_to :location
+	def change
+		create_table :users do |t|
+			t.string :username
+			t.string :password_digest
+			t.boolean :admin
+			t.belongs_to :location
 
-      t.timestamps
-    end
+			t.timestamps
+		end
 
-    add_index :users, [:username, :location_id]
-  end
+		add_index :users, [:username, :location_id]
+	end
 end

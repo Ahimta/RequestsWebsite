dc483e12d5a9cf479dcf880ee2bcdb8e77152195
Abdullah Alansari <ahimta@gmail.com>
2013-11-21 03:41:55 +0300

    Gemfile now doesn't specify ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -9,113 +9,113 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602183153) do
+ActiveRecord::Schema.define(version: 20130602183153) do
 
-  create_table "alerts", :force => true do |t|
+  create_table "alerts", force: true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "applicants", :force => true do |t|
+  create_table "applicants", force: true do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "applicants", ["user_id"], :name => "index_applicants_on_user_id"
+  add_index "applicants", ["user_id"], name: "index_applicants_on_user_id"
 
-  create_table "comings", :force => true do |t|
+  create_table "comings", force: true do |t|
     t.string   "reason"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "companions", :force => true do |t|
+  create_table "companions", force: true do |t|
     t.string   "name"
     t.string   "relation"
     t.string   "birthdate"
     t.integer  "ticket_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "companions", ["ticket_id"], :name => "index_companions_on_ticket_id"
+  add_index "companions", ["ticket_id"], name: "index_companions_on_ticket_id"
 
-  create_table "decisions", :force => true do |t|
+  create_table "decisions", force: true do |t|
     t.integer  "number"
     t.integer  "request_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "decisions", ["request_id"], :name => "index_decisions_on_request_id"
+  add_index "decisions", ["request_id"], name: "index_decisions_on_request_id"
 
-  create_table "leaves", :force => true do |t|
+  create_table "leaves", force: true do |t|
     t.string   "reason"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "locations", :force => true do |t|
+  create_table "locations", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "passports", :force => true do |t|
+  create_table "passports", force: true do |t|
     t.integer  "passportable_id"
     t.string   "passportable_type"
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "passports", ["passportable_id", "passportable_type"], :name => "index_passports_on_passportable_id_and_passportable_type"
+  add_index "passports", ["passportable_id", "passportable_type"], name: "index_passports_on_passportable_id_and_passportable_type"
 
-  create_table "requests", :force => true do |t|
+  create_table "requests", force: true do |t|
     t.boolean  "accepted"
     t.integer  "applicant_id"
     t.integer  "requestable_id"
     t.string   "requestable_type"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "requests", ["applicant_id"], :name => "index_requests_on_applicant_id"
-  add_index "requests", ["requestable_id", "requestable_type"], :name => "index_requests_on_requestable_id_and_requestable_type"
+  add_index "requests", ["applicant_id"], name: "index_requests_on_applicant_id"
+  add_index "requests", ["requestable_id", "requestable_type"], name: "index_requests_on_requestable_id_and_requestable_type"
 
-  create_table "tickets", :force => true do |t|
+  create_table "tickets", force: true do |t|
     t.string   "line"
     t.string   "number"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "username"
     t.string   "password_digest"
     t.boolean  "admin"
     t.integer  "location_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["location_id"], :name => "index_users_on_location_id"
+  add_index "users", ["location_id"], name: "index_users_on_location_id"
 
-  create_table "vacations", :force => true do |t|
+  create_table "vacations", force: true do |t|
     t.integer  "duration"
     t.string   "from"
     t.string   "to"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
